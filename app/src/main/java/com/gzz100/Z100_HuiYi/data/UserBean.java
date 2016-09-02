c6db504e9ec86bea349869c21212b0b56e266689
起个顺心的名字吧 <78>
2016-09-02 14:52:08 +0800

    用户实体类，暂定

---
+++ app/src/main/java/com/gzz100/Z100_HuiYi/data/UserBean.java
@@ -0,0 +1,34 @@
+package com.gzz100.Z100_HuiYi.data;
+
+/**
+ * Created by XieQXiong on 2016/9/2.
+ */
+public class UserBean {
+    private int userId;
+    private String userName;
+    private String userTitle;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserTitle() {
+        return userTitle;
+    }
+
+    public void setUserTitle(String userTitle) {
+        this.userTitle = userTitle;
+    }
+}
