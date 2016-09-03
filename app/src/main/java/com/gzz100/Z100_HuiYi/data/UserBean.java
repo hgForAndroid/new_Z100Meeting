1bea5ed593dc16960ff41659e2822f37d9b762a8
起个顺心的名字吧 <78>
2016-09-03 21:23:22 +0800

    参会人员实体类，命名上还得讨论，先暂定

--- app/src/main/java/com/gzz100/Z100_HuiYi/data/UserBean.java
+++ app/src/main/java/com/gzz100/Z100_HuiYi/data/UserBean.java
@@ -1,5 +1,11 @@
 package com.gzz100.Z100_HuiYi.data;
 
+import android.content.Context;
+import android.widget.ImageView;
+
+import com.gzz100.Z100_HuiYi.R;
+import com.squareup.picasso.Picasso;
+
 /**
  * Created by XieQXiong on 2016/9/2.
  */
@@ -7,6 +13,14 @@ public class UserBean {
     private int userId;
     private String userName;
     private String userTitle;
+    private int type;
+
+    public UserBean(int userId, String userName, String userTitle, int type) {
+        this.userId = userId;
+        this.userName = userName;
+        this.userTitle = userTitle;
+        this.type = type;
+    }
 
     public int getUserId() {
         return userId;
@@ -30,5 +44,24 @@ public class UserBean {
 
     public void setUserTitle(String userTitle) {
         this.userTitle = userTitle;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public void setPicForUser(Context context, ImageView imageView){
+        if (getType() == 1){//主持人
+            Picasso.with(context).load(R.mipmap.ic_launcher).into(imageView);
+        }else if (getType() == 2){//主讲人
+            Picasso.with(context).load(R.mipmap.ic_launcher).into(imageView);
+        }else {//其他参会人员
+            Picasso.with(context).load(R.mipmap.ic_launcher).into(imageView);
+        }
+
     }
 }
