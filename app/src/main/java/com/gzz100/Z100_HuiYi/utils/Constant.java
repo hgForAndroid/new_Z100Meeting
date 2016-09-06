10f5511f5381bccda420fab2b250a22380f15ba8
起个顺心的名字吧 <78>
2016-09-06 15:55:35 +0800

    常量类

---
+++ app/src/main/java/com/gzz100/Z100_HuiYi/utils/Constant.java
@@ -0,0 +1,14 @@
+package com.gzz100.Z100_HuiYi.utils;
+/**
+* 常量类
+* @author XieQXiong
+* create at 2016/9/6 10:42
+*/
+
+public class Constant {
+    public static final int NORMAL_DEGELATE = 1;
+    public static final int KEYNOTE_SPEAKER = 2;
+    public static final int HOST = 3;
+
+    public static final boolean DEBUG = true;
+}
