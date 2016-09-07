a05923b4baefb6e6767e97f17f0d3956bbbeccf5
xie <877028398@qq.com>
2016-09-07 17:23:29 +0800

    会议Presenter,改用会议仓库根据本地是否有数据进行数据请求的分发

--- app/src/main/java/com/gzz100/Z100_HuiYi/meeting/meetingScenario/MeetingPresenter.java
+++ app/src/main/java/com/gzz100/Z100_HuiYi/meeting/meetingScenario/MeetingPresenter.java
@@ -1,17 +1,25 @@
 package com.gzz100.Z100_HuiYi.meeting.meetingScenario;
 
+import android.support.annotation.NonNull;
+
 import com.gzz100.Z100_HuiYi.data.UserBean;
+import com.gzz100.Z100_HuiYi.data.meeting.MeetingDataSource;
+import com.gzz100.Z100_HuiYi.data.meeting.MeetingRepository;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
  * Created by XieQXiong on 2016/9/5.
  */
 public class MeetingPresenter implements MeetingContract.Presenter {
     private MeetingContract.View mView;
-    public MeetingPresenter(MeetingContract.View view) {
-        this.mView = view;
+    private MeetingRepository mMeetingRepository;
+    public MeetingPresenter(@NonNull MeetingRepository meetingRepository, @NonNull MeetingContract.View view) {
+        this.mMeetingRepository = checkNotNull(meetingRepository,"meetingRepository cannot be null");
+        this.mView = checkNotNull(view,"view cannot be null");
         this.mView.setPresenter(this);
     }
 
@@ -35,33 +43,21 @@ public class MeetingPresenter implements MeetingContract.Presenter {
     }
 
     boolean firstLoad = true;
-    private List<UserBean> mUsers = new ArrayList<>();
     @Override
     public void fetchMainUsers() {
         if (firstLoad){
             firstLoad = false;
-            UserBean user1 = new UserBean(1,"张三张三","android开发",2);
-            UserBean user2 = new UserBean(2,"王麻子","Java开发",2);
-            UserBean user3 = new UserBean(3,"临到","iOS开发",2);
-            UserBean user4 = new UserBean(4,"李四","c++开发",2);
-            UserBean user5 = new UserBean(5,"王五","c开发",2);
-            UserBean user6 = new UserBean(6,"王小二","c#开发",2);
-            UserBean user7 = new UserBean(7,"赵烈","技术支持",2);
-            UserBean user8 = new UserBean(8,"饼子","客服",2);
-            UserBean user9 = new UserBean(9,"胡子","经理",1);
-            UserBean user10 = new UserBean(10,"阿毛","助理",3);
-            mUsers.add(user1);
-            mUsers.add(user2);
-            mUsers.add(user3);
-            mUsers.add(user4);
-            mUsers.add(user5);
-        mUsers.add(user6);
-        mUsers.add(user7);
-        mUsers.add(user8);
-        mUsers.add(user9);
-        mUsers.add(user10);
+            mMeetingRepository.getDelegateList(new MeetingDataSource.LoadDelegateCallback() {
+                @Override
+                public void onDelegateLoaded(List<UserBean> users) {
+                    mView.showMeetingRoom(users);
 
-            mView.showMeetingRoom(mUsers);
+                }
+                @Override
+                public void onDataNotAvailable() {
+
+                }
+            });
         }
 
     }
