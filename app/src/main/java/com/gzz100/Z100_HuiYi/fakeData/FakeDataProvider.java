d1cb6ca20816b57e7039e5856a051f7c76cea379
xie <877028398@qq.com>
2016-09-07 17:25:09 +0800

    假数据，新增了用户集合

--- app/src/main/java/com/gzz100/Z100_HuiYi/fakeData/FakeDataProvider.java
+++ app/src/main/java/com/gzz100/Z100_HuiYi/fakeData/FakeDataProvider.java
@@ -2,6 +2,7 @@ package com.gzz100.Z100_HuiYi.fakeData;
 
 import com.gzz100.Z100_HuiYi.data.Agenda;
 import com.gzz100.Z100_HuiYi.data.Document;
+import com.gzz100.Z100_HuiYi.data.UserBean;
 import com.gzz100.Z100_HuiYi.data.Vote;
 
 import java.util.ArrayList;
@@ -54,7 +55,7 @@ public class FakeDataProvider {
             case 2:
                 for (int i = 1; i < 5; i++) {
                     Document document = new Document();
-                    document.setAgendaIndex(1 + "");
+                    document.setAgendaIndex(2 + "");
                     document.setFileURL("agenda_two_address" + i);
                     document.setFileIndex(i + "");
                     document.setFileName("agenda_two_fileName.doc" + i);
@@ -67,7 +68,7 @@ public class FakeDataProvider {
             case 3:
                 for (int i = 1; i < 5; i++) {
                     Document document = new Document();
-                    document.setAgendaIndex(1 + "");
+                    document.setAgendaIndex(3 + "");
                     document.setFileURL("agenda_three_address" + i);
                     document.setFileIndex(i + "");
                     document.setFileName("agenda_three_fileName.xlsx" + i);
@@ -123,5 +124,30 @@ public class FakeDataProvider {
         }
 
         return vote;
+    }
+
+    public static List<UserBean> getUsers(){
+        List<UserBean> mUsers = new ArrayList<>();
+        UserBean user1 = new UserBean(1,"张三张三","android开发",2);
+        UserBean user2 = new UserBean(2,"王麻子","Java开发",2);
+        UserBean user3 = new UserBean(3,"临到","iOS开发",2);
+        UserBean user4 = new UserBean(4,"李四","c++开发",2);
+        UserBean user5 = new UserBean(5,"王五","c开发",2);
+        UserBean user6 = new UserBean(6,"王小二","c#开发",2);
+        UserBean user7 = new UserBean(7,"赵烈","技术支持",2);
+        UserBean user8 = new UserBean(8,"饼子","客服",2);
+        UserBean user9 = new UserBean(9,"胡子","经理",1);
+        UserBean user10 = new UserBean(10,"阿毛","助理",3);
+        mUsers.add(user1);
+        mUsers.add(user2);
+        mUsers.add(user3);
+        mUsers.add(user4);
+        mUsers.add(user5);
+        mUsers.add(user6);
+        mUsers.add(user7);
+        mUsers.add(user8);
+        mUsers.add(user9);
+        mUsers.add(user10);
+        return mUsers;
     }
 }
