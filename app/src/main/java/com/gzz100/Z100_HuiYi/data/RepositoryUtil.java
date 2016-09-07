f67eed421580804f7ba71f065bb5e48a728be88d
xie <877028398@qq.com>
2016-09-07 17:27:28 +0800

    仓库工具，提供各种仓库类

---
+++ app/src/main/java/com/gzz100/Z100_HuiYi/data/RepositoryUtil.java
@@ -0,0 +1,33 @@
+package com.gzz100.Z100_HuiYi.data;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.gzz100.Z100_HuiYi.data.file.FileRepository;
+import com.gzz100.Z100_HuiYi.data.file.local.FileLocalDataSource;
+import com.gzz100.Z100_HuiYi.data.file.remote.FileRemoteDataSource;
+import com.gzz100.Z100_HuiYi.data.meeting.MeetingRepository;
+import com.gzz100.Z100_HuiYi.data.meeting.local.MeetingLocalDataSource;
+import com.gzz100.Z100_HuiYi.data.meeting.remote.MeetingRemoteDataSource;
+import com.gzz100.Z100_HuiYi.data.vote.VoteRepository;
+import com.gzz100.Z100_HuiYi.data.vote.local.VoteLocalDataSource;
+import com.gzz100.Z100_HuiYi.data.vote.remote.VoteRemoteDataSource;
+
+/**
+ * Created by XieQXiong on 2016/8/26.
+ */
+public class RepositoryUtil {
+    public static FileRepository getFileRepository(@NonNull Context context){
+        return FileRepository.getInstance(FileRemoteDataSource.getInstance(context),
+                FileLocalDataSource.getInstance(context),context);
+    }
+
+    public static VoteRepository getVoteRepository(@NonNull Context context){
+        return VoteRepository.getInstance(VoteRemoteDataSource.getInstance(context),
+                VoteLocalDataSource.getInstance(context));
+    }
+    public static MeetingRepository getMeetingRepository(@NonNull Context context){
+        return MeetingRepository.getInstance(MeetingRemoteDataSource.getInstance(context),
+                MeetingLocalDataSource.getInstance(context),context);
+    }
+}
