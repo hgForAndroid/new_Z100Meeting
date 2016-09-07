9e62c86a2c00603c7b9d6658ebbda21108b77c68
xie <877028398@qq.com>
2016-09-07 17:22:18 +0800

    常量类

--- app/src/main/java/com/gzz100/Z100_HuiYi/utils/Constant.java
+++ app/src/main/java/com/gzz100/Z100_HuiYi/utils/Constant.java
@@ -6,9 +6,13 @@ package com.gzz100.Z100_HuiYi.utils;
 */
 
 public class Constant {
+    //主场景会议桌中显示的各个参会人员的类型
     public static final int NORMAL_DEGELATE = 1;
     public static final int KEYNOTE_SPEAKER = 2;
     public static final int HOST = 3;
-
+    //测试控制
     public static final boolean DEBUG = true;
+    //议程列表存储数据库的对应字段值，用于查询数据库中是否有议程列表
+    public static final int COLUMNS_AGENDAS = 4;
+    public static final int COLUMNS_USER = 5;
 }
