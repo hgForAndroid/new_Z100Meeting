4061b8d5fcdbda12ad9a0b96db941650ec5093f5
xie <877028398@qq.com>
2016-09-07 17:34:42 +0800

    数据库帮助类，已经将之前该类中的向数据库中插入数据，读取数据的操作分离出去，在文件操作类，还有会议操作类中。

---
+++ app/src/main/java/com/gzz100/Z100_HuiYi/data/db/DBHelper.java
@@ -0,0 +1,65 @@
+package com.gzz100.Z100_HuiYi.data.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+* 数据库
+* @author XieQXiong
+* create at 2016/9/7 15:22
+*/
+
+public class DBHelper extends SQLiteOpenHelper {
+    private static final int DB_VERSION = 1;
+    private  static final String DB_NAME = "meetingData.db";
+    private static final String TEXT_TYPE = " TEXT";
+    private static final String INTEGER_TYPE = " INTEGER";
+    private static final String COMMA_SEP = ",";
+
+    private static final String CREATE_TABLE =
+            "CREATE TABLE " + PersistenceContract.ColumnsName.TABLE_NAME+ " (" +
+            PersistenceContract.ColumnsName._ID + TEXT_TYPE + " PRIMARY KEY,"  +
+            PersistenceContract.ColumnsName.COLUMN_NAME_AGENDA_INDEX + INTEGER_TYPE
+            + COMMA_SEP + PersistenceContract.ColumnsName.COLUMN_NAME_FILE_LIST + TEXT_TYPE
+            + COMMA_SEP + PersistenceContract.ColumnsName.COLUMN_NAME_AGENDAS + INTEGER_TYPE
+            + COMMA_SEP + PersistenceContract.ColumnsName.COLUMN_NAME_AGENDA_LIST + TEXT_TYPE
+            + COMMA_SEP + PersistenceContract.ColumnsName.COLUMN_NAME_USERS + INTEGER_TYPE
+            + COMMA_SEP + PersistenceContract.ColumnsName.COLUMN_NAME_USERS_LIST + TEXT_TYPE
+            + ")";
+    private SQLiteDatabase mDatabase;
+
+    private DBHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+    private static DBHelper instance;
+
+    public static DBHelper getInstance(Context context){
+        if (instance == null){
+            synchronized (DBHelper.class){
+                if (instance == null){
+                    instance = new DBHelper(context);
+                }
+            }
+        }
+        return instance;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE);
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    /**
+     * 删除表，在有新增议程时调用
+     */
+    public void deleteTable(){
+        mDatabase = instance.getReadableDatabase();
+        String sql = "DROP TABLE "+PersistenceContract.ColumnsName.TABLE_NAME;
+        mDatabase.execSQL(sql);
+    }
+}
