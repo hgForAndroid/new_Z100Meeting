46a4da3219f3284a3d339ffc4ec86730b1110c64
xie <877028398@qq.com>
2016-09-08 15:37:53 +0800

    假数据，新增会议概况弹窗数据

--- app/src/main/java/com/gzz100/Z100_HuiYi/fakeData/FakeDataProvider.java
+++ app/src/main/java/com/gzz100/Z100_HuiYi/fakeData/FakeDataProvider.java
@@ -3,6 +3,7 @@ package com.gzz100.Z100_HuiYi.fakeData;
 import com.gzz100.Z100_HuiYi.data.Agenda;
 import com.gzz100.Z100_HuiYi.data.DelegateBean;
 import com.gzz100.Z100_HuiYi.data.Document;
+import com.gzz100.Z100_HuiYi.data.MeetingInfo;
 import com.gzz100.Z100_HuiYi.data.UserBean;
 import com.gzz100.Z100_HuiYi.data.Vote;
 
@@ -218,5 +219,10 @@ public class FakeDataProvider {
         mUsers.add(user9);
         mUsers.add(user10);
         return mUsers;
+    }
+
+    public static MeetingInfo getMeetingInfo(){
+        MeetingInfo meetingInfo = new MeetingInfo("这是会议名称","2016.09.08   10:00","120",15,10,5);
+        return meetingInfo;
     }
 }
