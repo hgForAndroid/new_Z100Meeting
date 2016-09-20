10a12da842cc8827cd3f1c3256709a819e3a18fe
xie <877028398@qq.com>
2016-09-20 15:38:00 +0800

    文件界面，显示搜索结果每个item的监听接口

---
+++ app/src/main/java/com/gzz100/Z100_HuiYi/meeting/file/OnSearchItemClickListener.java
@@ -0,0 +1,12 @@
+package com.gzz100.Z100_HuiYi.meeting.file;
+
+/**
+ * Created by XieQXiong on 2016/9/20.
+ */
+public interface OnSearchItemClickListener {
+    /**
+     * 搜索结果，每个文件的点击监听
+     * @param position  文件序号
+     */
+    void onSearchClick(int position);
+}
