ba337a0d6461961bdfcd8668afb61b641372a268
xie <877028398@qq.com>
2016-09-22 16:47:47 +0800

    会议的点击监听

---
+++ app/src/main/java/com/gzz100/Z100_HuiYi/meetingPrepare/selectMeeting/OnMeetingClickListener.java
@@ -0,0 +1,12 @@
+package com.gzz100.Z100_HuiYi.meetingPrepare.selectMeeting;
+
+/**
+ * Created by XieQXiong on 2016/9/22.
+ */
+public interface OnMeetingClickListener {
+    /**
+     * 点击某个会议的监听事件
+     * @param position   会议序号
+     */
+    void onMeetingClick(int position);
+}
