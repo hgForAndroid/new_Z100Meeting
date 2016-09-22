d59edc2aa10fa7ab921710ebc150ef1c96a3e1b8
xie <877028398@qq.com>
2016-09-22 16:55:00 +0800

    从服务器获取会议列表等操作的接口类

---
+++ app/src/main/java/com/gzz100/Z100_HuiYi/data/selectMeeting/SelectMeetingDataSource.java
@@ -0,0 +1,25 @@
+package com.gzz100.Z100_HuiYi.data.selectMeeting;
+
+import android.support.annotation.NonNull;
+
+import com.gzz100.Z100_HuiYi.data.MeetingBean;
+
+import java.util.List;
+
+/**
+ * Created by XieQXiong on 2016/9/22.
+ */
+public interface SelectMeetingDataSource {
+    interface LoadMeetingListCallback{
+        void onMeetingListLoaded(List<MeetingBean> meetings);
+        void onDataNotAvailable();
+    }
+
+    /**
+     * 获取会议列表，目前需传什么参数未知
+     * @param callback    返回结果的回调
+     */
+    void fetchMeetingList( @NonNull LoadMeetingListCallback callback);
+
+//    void selectMeeting();
+}
