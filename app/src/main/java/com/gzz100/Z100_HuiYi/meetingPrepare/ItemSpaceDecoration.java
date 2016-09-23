32b071c8709c381541539940dda1f10ab5b89653
xie <877028398@qq.com>
2016-09-23 15:20:13 +0800

    RecyclerView每个Item添加间隔距离，可以传入横向还是纵向

---
+++ app/src/main/java/com/gzz100/Z100_HuiYi/meetingPrepare/ItemSpaceDecoration.java
@@ -0,0 +1,41 @@
+package com.gzz100.Z100_HuiYi.meetingPrepare;
+
+import android.graphics.Rect;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.LinearLayout;
+
+/**
+ * Created by XieQXiong on 2016/9/23.
+ */
+public class ItemSpaceDecoration extends RecyclerView.ItemDecoration {
+    public static final int HORIZONTAL = LinearLayout.HORIZONTAL;
+    public static final int VERTICAL = LinearLayout.VERTICAL;
+
+    private int orientation;
+
+    private int space;
+
+    public ItemSpaceDecoration(int orientation, int space) {
+        setOrientation(orientation);
+        this.space = space;
+    }
+
+    private void setOrientation(int orientation) {
+        if (orientation != HORIZONTAL && orientation != VERTICAL) {
+            throw new IllegalArgumentException("invalid orientation:" + orientation);
+        }
+        this.orientation = orientation;
+    }
+
+    @Override
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+        super.getItemOffsets(outRect, view, parent, state);
+        if (this.orientation == HORIZONTAL){
+            outRect.right = space;
+        }else if (this.orientation == VERTICAL){
+            outRect.bottom = space;
+        }
+    }
+
+}
