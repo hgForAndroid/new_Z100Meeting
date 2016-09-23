01b59ce8602698fa9581463a35a9d512fdd1103d
xie <877028398@qq.com>
2016-09-23 15:18:14 +0800

    添加会议列表的间隔

--- app/src/main/java/com/gzz100/Z100_HuiYi/meetingPrepare/selectMeeting/MeetingAdapter.java
+++ app/src/main/java/com/gzz100/Z100_HuiYi/meetingPrepare/selectMeeting/MeetingAdapter.java
@@ -1,6 +1,7 @@
 package com.gzz100.Z100_HuiYi.meetingPrepare.selectMeeting;
 
 import android.content.Context;
+import android.graphics.Rect;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
