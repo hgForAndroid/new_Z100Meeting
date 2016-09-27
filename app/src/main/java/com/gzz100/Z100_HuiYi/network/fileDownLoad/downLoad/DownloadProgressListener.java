d1e21d73d78a739cf8513d60089d194350d20002
xie <877028398@qq.com>
2016-09-27 16:15:42 +0800

    文件下载进度监听器

--- app/src/main/java/com/gzz100/Z100_HuiYi/network/fileDownLoad/downLoad/DownloadProgressListener.java
+++ app/src/main/java/com/gzz100/Z100_HuiYi/network/fileDownLoad/downLoad/DownloadProgressListener.java
@@ -1,9 +1,11 @@
 package com.gzz100.Z100_HuiYi.network.fileDownLoad.downLoad;
 
 /**
- * 下载进度listener
- * Created by JokAr on 16/5/11.
- */
+* 文件下载进度监听器
+* @author XieQXiong
+* create at 2016/9/27 16:15
+*/
+
 public interface DownloadProgressListener {
     void update(long bytesRead, long contentLength, boolean done);
 }
