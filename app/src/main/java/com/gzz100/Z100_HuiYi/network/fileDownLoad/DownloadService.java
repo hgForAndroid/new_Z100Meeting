bd182b90d26aa6bff16abd473631e41c6c389096
xie <877028398@qq.com>
2016-09-27 16:35:43 +0800

    下载文件的接口，这里采用全路径下载

---
+++ app/src/main/java/com/gzz100/Z100_HuiYi/network/fileDownLoad/DownloadService.java
@@ -0,0 +1,19 @@
+package com.gzz100.Z100_HuiYi.network.fileDownLoad;
+
+import okhttp3.ResponseBody;
+import retrofit2.http.GET;
+import retrofit2.http.Streaming;
+import retrofit2.http.Url;
+import rx.Observable;
+/**
+* @author XieQXiong
+* create at 2016/9/27 16:20
+*/
+
+
+public interface DownloadService {
+    
+    @Streaming
+    @GET
+    Observable<ResponseBody> download(@Url String url);
+}
