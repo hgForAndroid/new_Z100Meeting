267dbee658af671f210c35004d13f2667e1a27be
xie <877028398@qq.com>
2016-09-27 16:14:28 +0800

    文件下载进度监听器

---
+++ app/src/main/java/com/gzz100/Z100_HuiYi/network/fileDownLoad/downLoad/DownloadProgressListener.java
@@ -0,0 +1,9 @@
+package com.gzz100.Z100_HuiYi.network.fileDownLoad.downLoad;
+
+/**
+ * 下载进度listener
+ * Created by JokAr on 16/5/11.
+ */
+public interface DownloadProgressListener {
+    void update(long bytesRead, long contentLength, boolean done);
+}
