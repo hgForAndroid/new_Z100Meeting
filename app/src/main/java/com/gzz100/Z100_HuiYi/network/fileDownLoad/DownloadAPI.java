e80f9f397dc624daa7addda57f0010c3c616d642
xie <877028398@qq.com>
2016-11-10 16:02:52 +0800

    下载管理器

--- app/src/main/java/com/gzz100/Z100_HuiYi/network/fileDownLoad/DownloadAPI.java
+++ app/src/main/java/com/gzz100/Z100_HuiYi/network/fileDownLoad/DownloadAPI.java
@@ -1,86 +1,20 @@
 package com.gzz100.Z100_HuiYi.network.fileDownLoad;
 
-import android.support.annotation.NonNull;
-import android.util.Log;
-
-import com.gzz100.Z100_HuiYi.network.fileDownLoad.downLoad.DownloadProgressInterceptor;
-import com.gzz100.Z100_HuiYi.network.fileDownLoad.downLoad.DownloadProgressListener;
-import com.gzz100.Z100_HuiYi.utils.Constant;
-import com.gzz100.Z100_HuiYi.utils.FileUtils;
-
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.concurrent.TimeUnit;
-
-import okhttp3.OkHttpClient;
 import okhttp3.ResponseBody;
-import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
-import rx.Subscriber;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.functions.Action1;
-import rx.functions.Func1;
-import rx.schedulers.Schedulers;
-
+import retrofit2.http.GET;
+import retrofit2.http.Streaming;
+import retrofit2.http.Url;
+import rx.Observable;
 /**
- * @author XieQXiong
- *         create at 2016/9/27 16:20
- */
+ * 下载文件的接口
+* @author XieQXiong
+* create at 2016/9/27 16:36
+*/
 
 
-public class DownloadAPI {
-    private static final String TAG = "DownloadAPI";
-    private static final int DEFAULT_TIMEOUT = 15;
-    public Retrofit retrofit;
-
-
-    public DownloadAPI(String url, DownloadProgressListener listener) {
-
-        DownloadProgressInterceptor interceptor = new DownloadProgressInterceptor(listener);
-
-        OkHttpClient client = new OkHttpClient.Builder()
-                .addInterceptor(interceptor)
-                .retryOnConnectionFailure(true)
-                .connectTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS)
-                .build();
-
-
-        retrofit = new Retrofit.Builder()
-                .baseUrl(url)
-                .client(client)
-                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-                .build();
-    }
-
-    public void downloadFile(@NonNull String url, final File file, Subscriber subscriber) {
-        if (Constant.DEBUG)
-            Log.d(TAG, "downloadFile: " + url);
-
-        retrofit.create(DownloadService.class)
-                .download(url)
-                .subscribeOn(Schedulers.io())
-                .unsubscribeOn(Schedulers.io())
-                .map(new Func1<ResponseBody, InputStream>() {
-                    @Override
-                    public InputStream call(ResponseBody responseBody) {
-                        return responseBody.byteStream();
-                    }
-                })
-                .observeOn(Schedulers.computation())
-                .doOnNext(new Action1<InputStream>() {
-                    @Override
-                    public void call(InputStream inputStream) {
-                        try {
-                            FileUtils.writeFile(inputStream, file);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                })
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(subscriber);
-    }
-
-
+public interface DownloadApi {
+    
+    @Streaming
+    @GET
+    Observable<ResponseBody> download(@Url String url);
 }
